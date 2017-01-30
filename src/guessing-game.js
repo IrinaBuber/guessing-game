4e2621573941fbf37d1c3ff1f97bb32834d367b2
IrinaBuber <irina.buber@gmail.com>
2017-01-30 13:43:59 +0300

    implementation of GuessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this._min = this._max = this._lastGuess = 0;
+    }
 
     setRange(min, max) {
-
+    	this._min = min;
+    	this._max = max;
     }
 
     guess() {
-
+    	this._lastGuess = Math.ceil((this._max + this._min) / 2);
+    	return this._lastGuess;
     }
 
     lower() {
-
+    	this._max = this._lastGuess;
+    	this._lastGuess = Math.ceil((this._max + this._min) / 2);
+    	return this._lastGuess;
     }
 
     greater() {
-
+    	this._min = this._lastGuess;
+    	this._lastGuess = Math.ceil((this._max + this._min) / 2);
+    	return this._lastGuess;
     }
 }
 
